bbc872a9f4f8aeafdcb6dac7552464a9a53f68f5
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-12 20:04:51 +0530

    Add files via upload

---
+++ JAVA/Typecasting.java
@@ -0,0 +1,11 @@
+package Learning;
+
+public class Typecasting{
+    public static void main(String[] args)
+    {
+        float b=45.66f;
+        int a=(int) b;
+        System.out.println(a);
+    }
+    
+}
