c083d4899453388bed6530764917455840996538
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-12 20:08:02 +0530

    Add files via upload

---
+++ JAVA/String_1.java
@@ -0,0 +1,10 @@
+package Learning;
+
+public class String_1{
+    public static void main(String args[])
+    {
+        String name=new String("Tushar");
+        System.out.println(name);
+    }
+    
+}
