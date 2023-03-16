9e7147a4952bed3653c82adaa64420a13674a4d5
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-16 18:56:00 +0530

    Create adding_two_strings.java

---
+++ JAVA/adding_two_strings.java
@@ -0,0 +1,16 @@
+package Learning;
+import java.util.*;
+
+public class adding_two_strings {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String F_name=sc.nextLine();
+        System.out.println("Enter Last Name: ");
+        String L_name=sc.nextLine();
+        System.out.println(F_name +" "+L_name);
+        sc.close();
+    }
+
+}
