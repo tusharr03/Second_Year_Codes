9180075dd2b28f047271a594c67c5c821ffb417c
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-18 20:46:36 +0530

    Create Nested_loops_2.c

---
+++ C/Nested_loops_2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    {
+    for(j=0;j<=7;j++;)
+    {
+        for(i=1;i<=j;i++;)
+        
+        printf("%d",i);
+    }
+    printf("\n");
+    }
+    
+    return 0;
+    
+}
