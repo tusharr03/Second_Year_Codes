e6937a28fa444e9a74baff000e523b52f22c4925
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-19 19:02:19 +0000

    Create Basic_operators.c

---
+++ C/Basic_operators.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    int b=5;
+    
+    printf("the value of a+b is %d \n",a+b);
+    printf("the value of function is %d \n",a/b+a);
+    printf("the value of function is %d",a%b+b);
+    
+    return 0;
+}
