73c7e38b460cfe499df7c5c3d23ab6eefdb4b693
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-19 19:03:24 +0000

    Create Maximum_of_three_integers.c

---
+++ C/Maximum_of_three_integers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three numbers: \n");
+    scanf("%d%d%d",&a,&b,&c);
+    
+    if(a>b)
+    {
+        if(a>c)
+        printf("%d is maximum ",a);
+        
+        else
+        printf("%d is maximum",c);
+    }
+    
+    else{
+        if(b>c)
+        printf("%d is maximum",b);
+        
+        else
+        printf("%d is maximum",c);
+    } 
+    
+    return 0;
+    
+}
