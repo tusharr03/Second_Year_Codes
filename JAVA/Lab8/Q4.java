ebab2212d9e272fa8ce74e110dda65b14166a2b1
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-20 23:24:18 +0530

    Create Q4.java

---
+++ JAVA/Lab8/Q4.java
@@ -0,0 +1,9 @@
+package Lab8;
+
+import Lab8.Marketing.*;
+public class Q4 {
+    public static void main(String[] args) {
+        Sales ob = new Sales(1234 , "Tushar");
+        ob.tallowance();
+    }
+}
