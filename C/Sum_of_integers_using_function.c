9fb1f561160b29e851fb11c3500fa62772a420c1
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-22 19:58:56 +0530

    Create Sum_of_integers_using_function.c

---
+++ C/Sum_of_integers_using_function.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int sum(int a ,int b)
+
+{
+    return a + b;
+}
+
+
+int main()
+{
+    int a , b , c;
+    a = 5;
+    b = 6;
+    c = sum(a,b);
+    
+    printf("the sum is %d \n ", c);
+    
+    
+    return 0;
+}
