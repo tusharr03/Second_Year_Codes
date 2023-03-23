19802b52089dbf108efb7e0ad2257a2661944891
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-23 20:28:11 +0530

    Create Assignment_operator.c

---
+++ C/Assignment_operator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+   int a = 4,b=1;
+   b=a;
+   printf("value of b is %d \n",b);
+   b+=a;
+   printf("value of b is %d \n",b);
+   b*=a;
+   printf("value of b is %d \n",b);
+   b/=a;
+   printf("value of b is %d \n",b);
+   b-=a;
+   printf("value of b is %d \n",b);
+   
+   
+    
+    return 0;
+}
