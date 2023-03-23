f33326006dade305b001c0519c8088e50944a1ec
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-23 20:25:37 +0530

    Create Addition_of_two_time.c

---
+++ C/Addition_of_two_time.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   int hrs,min,sec;
+    printf("enter the first time: \n");
+    scanf("%d%d%d",&hrs,&min,&sec);
+   
+    printf("enter the second time: \n");
+    scanf("%d%d%d",&hrs,&min,&sec);
+
+    
+    return 0;
+}
