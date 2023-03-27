d405f18e4db524463db36bd0a2c54b3540ef668e
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-27 13:15:52 +0530

    Update Lab_09_q2.java

--- JAVA/Lab9/Lab_09_q2.java
+++ JAVA/Lab9/Lab_09_q2.java
@@ -5,12 +5,16 @@ public class Lab_09_q2
         String s1="Tushar";
         String s2="sharma";
         s1=s1.toLowerCase();
+        System.out.println("String in lower case:");
         System.out.println(s1);
         s2=s2.toUpperCase();
+        System.out.println("String in Upper case:");
         System.out.println(s2);
         StringBuffer sb=new StringBuffer("Tushar");
         sb.reverse();
+        System.out.println("Reversed String:");
         System.out.println(sb);
+        System.out.println("Comparing Two Strings:");
         if(s1.equals(s2))
         {
             System.out.println("Both Strings are equal.");
@@ -19,7 +23,9 @@ public class Lab_09_q2
         {
             System.out.println("Strings are not equal");
         }
+        sb.reverse();
         sb.append(s2,0 ,3);
+        System.out.println("String after append:");
         System.out.println(sb);
         
 
